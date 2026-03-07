765c8a1b91864ef1f5b049892f5eb303a2c89cc0
Ruben <rubenfambuena42@gmail.com>
2026-03-07 11:02:28 +0100

    Color palette defined

---
+++ MyCustomComponents/src/main/java/com/rfambuena/mycustomcomponents/ui/theme/Color.kt
@@ -0,0 +1,34 @@
+package com.rfambuena.mycustomcomponents.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// core
+val Primary = Color(0xFFC67C4E)
+val Secondary = Color(0xFFEDD6C8)
+val FontColor = Color(0xFF313131)
+val Background = Color(0xFFF9F2ED)
+val Disabled = Color(0xFFE3E3E3)
+
+// destructive/error
+val E1000 = Color(0xFF1F0000)
+val E900 = Color(0xFF3D0000)
+val E800 = Color(0xFF5C0000)
+val E700 = Color(0xFF8A0000)
+val E600 = Color(0xFF9B0000)
+val E500 = Color(0xFFB60000)
+val E400 = Color(0xFFFF0000)
+val E300 = Color(0xFFFF3D3D)
+val E200 = Color(0xFFFF6F6F)
+val E100 = Color(0xFFFF9B9B)
+
+// success
+val S1000 = Color(0xFF001F00)
+val S900 = Color(0xFF003D00)
+val S800 = Color(0xFF005C00)
+val S700 = Color(0xFF008A00)
+val S600 = Color(0xFF009B00)
+val S500 = Color(0xFF00B600)
+val S400 = Color(0xFF00FF00)
+val S300 = Color(0xFF6DFA40)
+val S200 = Color(0xFF92FF72)
+val S100 = Color(0xFFBAFFA8)
