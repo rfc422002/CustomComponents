7d2ddbd83069c97c3dc99e7a0cfd0411182de125
Ruben <rubenfambuena42@gmail.com>
2026-03-07 11:45:24 +0100

    Sora font family added and applied to text styles

--- MyCustomComponents/src/main/java/com/rfambuena/mycustomcomponents/ui/theme/Type.kt
+++ MyCustomComponents/src/main/java/com/rfambuena/mycustomcomponents/ui/theme/Type.kt
@@ -1,41 +1,50 @@
 package com.rfambuena.mycustomcomponents.ui.theme
 
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rfambuena.mycustomcomponents.R
+
+val soraFont = FontFamily(
+    Font(R.font.sora_light, FontWeight.Light),
+    Font(R.font.sora_regular, FontWeight.Normal),
+    Font(R.font.sora_bold, FontWeight.Bold),
+    Font(R.font.sora_semi_bold, FontWeight.SemiBold)
+)
 
 val title100 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.SemiBold,
         fontSize = 25.sp
     )
 
 val title200 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp
     )
 
 val title300 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.SemiBold,
         fontSize = 32.sp
     )
 
 val body100 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
 
 val body200 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.Normal,
         fontSize = 21.sp
     )
@@ -43,7 +52,7 @@ val body200 =
 
 val label100 =
     TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = soraFont,
         fontWeight = FontWeight.Light,
         fontSize = 14.sp
     )
