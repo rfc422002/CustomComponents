03d23b6d52d8bb0071274a2c15afd56b7219b578
Fambu <rubenfc2002@gmail.com>
2026-03-24 19:07:33 +0100

    fix Shape.kt large shape

--- MyCustomComponents/src/main/java/com/rfambuena/mycustomcomponents/ui/components/shapes/Shape.kt
+++ MyCustomComponents/src/main/java/com/rfambuena/mycustomcomponents/ui/components/shapes/Shape.kt
@@ -7,5 +7,5 @@ import androidx.compose.ui.unit.dp
 val Shape = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(16.dp)
 )
